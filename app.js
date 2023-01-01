451d313422f89178f4848d9c35858ef8eff0f598
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2023-01-01 14:26:25 +0100

    adds error middleware

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.get('/', (req, res) => {
   res.send('hello bookstore');
 });
 
+// error handler middleware
+app.use((err, res, req, next) => {
+  const errorStatus = err.status || 500;
+  res.status(errorStatus).send('An error occured');
+  next();
+});
+
 
 app.listen(CONFIG.PORT, () => {
   console.log(`server is listening at ${CONFIG.PORT}`);
